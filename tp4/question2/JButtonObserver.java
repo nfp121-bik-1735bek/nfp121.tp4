d2dc7c0b5bf9412e0d8dede1473fcdfda7ac129b
FaresAntoun <60392581+FaresAntoun@users.noreply.github.com>
2021-07-04 23:38:30 +0300

    Add files via upload

--- tp4/question2/JButtonObserver.java
+++ tp4/question2/JButtonObserver.java
@@ -3,6 +3,8 @@ package question2;
 //import java.awt.event. // à compléter
 //import java.awt.event. // à compléter
 import java.awt.TextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
  * Décrivez votre classe JButtonObserver ici.
@@ -10,7 +12,7 @@ import java.awt.TextArea;
  * @author (votre nom)
  * @version (un numéro de version ou une date)
  */
-public class JButtonObserver { // à compléter
+public class JButtonObserver implements ActionListener{ // à compléter
 
     private String nom;
     private TextArea contenu;
@@ -36,8 +38,8 @@ public class JButtonObserver { // à compléter
      * @param à
      *            compléter
      */
-    public void action________/* à compléter */(/* à compléter */) {
-        String message = ""; // à compléter, inspirez-vous de l'applette de l'énoncé
+    public void actionPerformed/* à compléter */(ActionEvent ae) {
+        String message = String.format("observateur %s : clic du bouton %s",this.nom != null ?this.nom:"",ae.getActionCommand()); // à compléter, inspirez-vous de l'applette de l'énoncé
         contenu.append(message + "\n");
     }
 
